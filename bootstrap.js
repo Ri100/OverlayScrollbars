a3f151c6e4677a5003305abdddc74fc244c4e7ae
Ri100 <hr.apostolov@live.com>
2016-12-25 21:00:27 +0100

    v0.37 push
    v0.37 - fast and dirty fix for twitch.tv and soundcloud.com double
    styled scrollbars --tnx to github.com/Keith94

--- bootstrap.js
+++ bootstrap.js
@@ -2,10 +2,12 @@ var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getSe
 var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
 
 var sheet = ios.newURI('data:text/css;charset=utf-8,' + encodeURIComponent('\
+  @-moz-document domain("www.twitch.tv") {.tse-scrollbar {opacity: 0 !important;}} \
+  @-moz-document domain("soundcloud.com") {.g-scrollbar.g-scrollbar-vertical.g-scrollbar-visible {opacity: 0 !important;}} \
   @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul); \
   @namespace html url("http://www.w3.org/1999/xhtml"); \
   scrollbar { -moz-appearance:none !important; border-radius: 0px; background-color: transparent; background-image: none; padding: 0px; position: relative; z-index: 2147483647;} scrollbar[orient="vertical"] {-moz-margin-start: -10px; min-width: 10px; max-width: 10px;} scrollbar[orient="horizontal"] {margin-top: -10px; min-height: 10px; max-height: 10px;} scrollbar thumb {-moz-appearance: none !important; border-width: 0px !important; background-image: -moz-linear-gradient(top left, rgba(255,255,255,0.20), rgba(255,255,255,0.20), rgba(255,255,255,0.20)); background-color: rgba(0,0,0,0.35) !important; border-radius: 0px !important;} scrollbar thumb:hover {-moz-appearance: none !important; border-width: 0px !important; background-image: -moz-linear-gradient(top left, rgba(255,255,255,0.35), rgba(255,255,255,0.35), rgba(255,255,255,0.35)); background-color: rgba(0,0,0,0.55) !important; border-radius: 0px !important;} scrollbar thumb:active {-moz-appearance: none !important; border-width: 0px !important; background-image: -moz-linear-gradient(top left, rgba(255,255,255,0.35), rgba(255,255,255,0.35), rgba(255,255,255,0.35)); background-color: rgba(0,0,0,0.55) !important; border-radius: 0px !important;} scrollbar scrollbarbutton, scrollbar gripper {display: none;} \
-  '), null, null);
+    '), null, null);
 
 function install() {}
 function uninstall() {}
